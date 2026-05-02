02612fbf93acf1e10438d27ecc2be14fb01e5ece
kosmys122 <kosmys122@gmail.com>
2026-05-03 01:47:43 +0300

    completed task 2

---
+++ lib/flutter_create_hello_student.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: (Scaffold(
+        body: Text(
+          "Привет! Меня зовут Шапкин. Я студент лучшей группы ИСП-232.",
+        ),
+      )),
+    ),
+  );
+}
