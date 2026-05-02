be4b8d71761a3f829d7e8549d06ecb1e764d864f
kosmys122 <kosmys122@gmail.com>
2026-05-03 01:04:05 +0300

    Style text: size 32, white color

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.white,
+                Colors.blue,
+                Colors.red,
+              ],
+              begin: AlignmentGeometry.topStart,
+              end:AlignmentGeometry.bottomCenter,
+            ),
+          ),
+          child: Center(
+            child: Text("Hello world!",style: TextStyle(color: Colors.white,fontSize: 32),),
+          ),
+        ),
+      ),
+    ),
+  );
+}
